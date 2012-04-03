a0357ba31ad35e29839ad3d994d5d17cf2e4d0ff
Gaurav Arora <gaurav.arora@compuware.com>
2012-04-03 14:41:53 -0400

    Adding Constants

--- CalenderEventApp/src/com/calenderEvent/controller/States.java
+++ CalenderEventApp/src/com/calenderEvent/controller/States.java
@@ -4,16 +4,4 @@ public interface States {
 
 	public final String ApplicationName = "com.calenderEvent";
 
-	public final int overViewPage = 0;
-	public final int detailViewPage = 1;
-	public final int ACTIVITY_CREATE = 0;
-	public final int ACTIVITY_EDIT = 1;
-
-	public final int SEVERITY_DEBUG = 0;
-	public final int SEVERITY_VERBOSE = 1;
-	public final int SEVERITY_ERROR = 2;
-	public final int SEVERITY_WTF = 3;
-	public final int SEVERITY_INFO = 4;
-	public final int SEVERITY_WARN = 5;
-
 }
